bde29e47892af610641c2d1acee701a7db5ac4f3
Cheickby10 <ccoulibaly873@gmail.com>
2026-08-02 00:00:20 +0000

    Add configuration settings for Project Orion Predictor

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,20 @@
+"""
+Configuration de Project Orion Predictor
+Version 0.1
+"""
 
+# Elo
+INITIAL_RATING = 1500
+K_FACTOR = 32
+
+# Poisson
+MAX_GOALS = 15
+
+# Pondération des matchs
+LAST_5_MATCHS_WEIGHT = 0.60
+OLDER_MATCHS_WEIGHT = 0.40
+
+# Paramètres futurs
+HOME_ADVANTAGE = 0.0
+USE_XG = False
+USE_MONTE_CARLO = False
