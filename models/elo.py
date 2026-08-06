17b01f923645ea09d363e823ecbfb7cfc044b855
Cheickby10 <ccoulibaly873@gmail.com>
2026-08-06 12:30:05 +0000

    Refactor Elo system into EloSystem class
    Refactored Elo rating system into a class structure with methods for rating retrieval, expected score calculation, and match updates.

--- models/elo.py
+++ models/elo.py
@@ -1,32 +1,85 @@
 """
 Project Orion Predictor
 Classement Elo
-Version 0.1
+Version 0.1.1
 """
 
 INITIAL_RATING = 1500
 K_FACTOR = 32
 
 
-def expected_score(rating_a, rating_b):
-    return 1 / (1 + 10 ** ((rating_b - rating_a) / 400))
+class EloSystem:
 
+    def __init__(self):
+        self.ratings = {}
 
-def update_elo(rating_a, rating_b, goals_a, goals_b):
-    expected_a = expected_score(rating_a, rating_b)
-    expected_b = expected_score(rating_b, rating_a)
+    def get_rating(self, team):
 
-    if goals_a > goals_b:
-        score_a = 1
-        score_b = 0
-    elif goals_a < goals_b:
-        score_a = 0
-        score_b = 1
-    else:
-        score_a = 0.5
-        score_b = 0.5
+        if team not in self.ratings:
+            self.ratings[team] = INITIAL_RATING
 
-    new_rating_a = rating_a + K_FACTOR * (score_a - expected_a)
-    new_rating_b = rating_b + K_FACTOR * (score_b - expected_b)
+        return self.ratings[team]
 
-    return round(new_rating_a, 2), round(new_rating_b, 2)
+    def expected_score(self, rating_a, rating_b):
+
+        return 1 / (
+            1 + 10 ** ((rating_b - rating_a) / 400)
+        )
+
+    def update(self, home, away, home_goals, away_goals):
+
+        rating_home = self.get_rating(home)
+        rating_away = self.get_rating(away)
+
+        expected_home = self.expected_score(
+            rating_home,
+            rating_away
+        )
+
+        expected_away = self.expected_score(
+            rating_away,
+            rating_home
+        )
+
+        if home_goals > away_goals:
+            score_home = 1
+            score_away = 0
+
+        elif home_goals < away_goals:
+            score_home = 0
+            score_away = 1
+
+        else:
+            score_home = 0.5
+            score_away = 0.5
+
+        rating_home += K_FACTOR * (
+            score_home - expected_home
+        )
+
+        rating_away += K_FACTOR * (
+            score_away - expected_away
+        )
+
+        self.ratings[home] = round(
+            rating_home,
+            2
+        )
+
+        self.ratings[away] = round(
+            rating_away,
+            2
+        )
+
+    def process_matches(self, matches):
+
+        for match in reversed(matches):
+
+            self.update(
+                match["home_team"],
+                match["away_team"],
+                match["home_goals"],
+                match["away_goals"]
+            )
+
+        return self.ratings
